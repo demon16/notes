35e69402ddcc9e03ea5d7e22e711b3d8abdf1c58
simon <coutingstars1@163.com>
2018-12-11 01:44:12 +0800

    struct start

--- c/main.c
+++ c/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
+int factorial(int n){
+  int result = 1;
+  while (n > 0) {
+    result = result * n;
+    n = n - 1;
+  }
+  return n;
+}
+
+void test_for(int n){
+  for(int i = 1; i < n; i ++){
+    printf("%d", i);
+  }
+}
+
+void print_table(void){
+  printf("\n");
+  int i, j;
+  for(i = 1; i <= 9; i++){
+    for(j = 1; j <= i; j++)
+      printf("%d\t", i*j);
+    printf("\n");
+  }
+}
+
 int main(void){
   // printf("Hellow, World.\n");
   // char firstletter = 'a';
@@ -21,6 +46,8 @@ int main(void){
       printf("Unknown\n");
       break;
   }
+  test_for(5);
+  print_table();
   
   return 0;
 }
